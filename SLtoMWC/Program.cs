94a65cfd21c5e3689c08f11fe5ff22f9bad4b2b8
Jonathan Simon <simon.jonathan@gmail.com>
2018-06-17 17:41:26 -0400

    first commit

---
+++ SLtoMWC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SLtoMWC
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
